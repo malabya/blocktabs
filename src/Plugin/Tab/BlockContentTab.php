278a5283bb5e1013ac942111ac93e34caca7fd08
g089h515r806 <g089h515r806@gmail.com>
2016-02-14 18:47:16 +0800

    add first workable version.

---
+++ src/Plugin/Tab/BlockContentTab.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\blocktabs\Plugin\Tab\BlockContentTab.
+ */
+
+namespace Drupal\blocktabs\Plugin\Tab;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\blocktabs\ConfigurableTabBase;
+use Drupal\blocktabs\BlocktabsInterface;
+
+/**
+ * block content tab.
+ *
+ * @Tab(
+ *   id = "block_content_tab",
+ *   label = @Translation("block content tab"),
+ *   description = @Translation("block content tab.")
+ * )
+ */
+class BlockContentTab extends ConfigurableTabBase {
+
+  /**
+   * {@inheritdoc}
+   */
+   
+  public function addTab(BlocktabsInterface $blocktabs) {
+
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSummary() {
+    $summary = array(
+      '#theme' => 'tab_summary',
+      '#data' => $this->configuration,
+    );
+    $summary += parent::getSummary();
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return array(
+      'block_uuid' => NULL,
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['block_uuid'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Block uuid'),
+      '#default_value' => $this->configuration['block_uuid'],
+      '#required' => TRUE,
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+
+    $this->configuration['block_uuid'] = $form_state->getValue('block_uuid');
+  }
+  
+  public function getContent() {
+    $tab_content = '';
+    $block_uuid = $this->configuration['block_uuid'];
+	if(!empty($block_uuid)){
+    $block = \Drupal::entityManager()->loadEntityByUuid('block_content', $block_uuid);
+	
+    $block_render_array = \Drupal::entityManager()->getViewBuilder($block->getEntityTypeId())->view($block, 'default');
+    $tab_content = drupal_render($block_render_array);
+	}
+    return $tab_content;
+  }
+}
---
+++ src/Plugin/Tab/ViewsTab.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\blocktabs\Plugin\Tab\ViewsTab.
+ */
+
+namespace Drupal\blocktabs\Plugin\Tab;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\blocktabs\ConfigurableTabBase;
+use Drupal\blocktabs\BlocktabsInterface;
+
+/**
+ * views tab.
+ *
+ * @Tab(
+ *   id = "views_tab",
+ *   label = @Translation("views tab"),
+ *   description = @Translation("views tab.")
+ * )
+ */
+class ViewsTab extends ConfigurableTabBase {
+
+  /**
+   * {@inheritdoc}
+   */
+   
+  public function addTab(BlocktabsInterface $blocktabs) {
+
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSummary() {
+    $summary = array(
+      '#theme' => 'tab_summary',
+      '#data' => $this->configuration,
+    );
+    $summary += parent::getSummary();
+
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return array(
+      'view_name' => NULL,
+	  'view_display' => NULL,
+	  'view_arg' => NULL,
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form['view_name'] = array(
+      '#type' => 'textfield',
+      '#title' => t('view name'),
+      '#default_value' => $this->configuration['view_name'],
+      '#field_suffix' => ' ' ,
+      '#required' => TRUE,
+    );
+    $form['view_display'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Display'),
+      '#default_value' => $this->configuration['view_display'],
+      '#field_suffix' => ' ' ,
+      '#required' => TRUE,
+    );	
+    $form['view_arg'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Argument'),
+      '#default_value' => $this->configuration['view_arg'],
+      '#field_suffix' => ' ' ,
+
+    );		
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+
+    $this->configuration['view_name'] = $form_state->getValue('view_name');
+	$this->configuration['view_display'] = $form_state->getValue('view_display');
+	$this->configuration['view_arg'] = $form_state->getValue('view_arg');
+  }
+
+  /**
+   * {@inheritdoc}
+   */  
+  public function getContent() {
+    $tab_content = '';
+    $view_name = $this->configuration['view_name'];
+    $view_display = $this->configuration['view_display'];
+    $view_arg = !empty($this->configuration['view_arg']) ? $this->configuration['view_arg'] : NULL;
+    $tab_view = views_embed_view($view_name, $view_display, $view_arg);	
+	$tab_content =  drupal_render($tab_view);
+    return $tab_content;
+  }
+}
