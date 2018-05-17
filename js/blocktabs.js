d835c7c3e6cae8ab9d36ace3484c903ca304f38b
Ellen Hsu <410505763@qq.com>
2018-05-17 20:45:08 +0800

    coding standard：Trailing spaces not allowed.

--- js/blocktabs.js
+++ js/blocktabs.js
@@ -21,7 +21,7 @@
         if ($(this).hasClass('click')) {
           $(this).tabs({
             event: 'click'
-          });		
+          });
         }
         else {
           $(this).tabs({
@@ -30,7 +30,7 @@
         }
         if ($(this).hasClass('vertical')) {
           $(this).addClass('ui-tabs-vertical ui-helper-clearfix');
-          $(this).find('li').removeClass('ui-corner-top').addClass('ui-corner-left');			
+          $(this).find('li').removeClass('ui-corner-top').addClass('ui-corner-left');
         }
       });
     }
